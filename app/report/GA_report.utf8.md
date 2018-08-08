47d65ce5c1e741e96c8191c9a7d380701688ead9
Steven H. Ranney <sranney@staywell.com>
2018-08-08 07:29:36 -0600

    Running commit.

---
+++ app/report/GA_report.utf8.md
@@ -0,0 +1,27 @@
+---
+output: 
+  beamer_presentation:
+    theme: "SwCustom"
+title: "Super-basic test report"
+date: "2018-08-08"
+toc: FALSE
+classoption: aspectratio=169
+editor_options: 
+  chunk_output_type: console
+params:
+  data: NA
+---
+
+
+
+## Analytics Notes
+
+Test report.
+
+**This is for internal use and discussion only.**
+
+## simple plot
+
+
+\begin{center}\includegraphics[width=1.422\textheight,height=.8\textheight]{figure/daily-users-1} \end{center}
+
--- app/ui.R
+++ app/ui.R
@@ -11,36 +11,34 @@ library(shiny)
 library(googleAuthR)
 library(dplyr)
 
-clients <- read.csv("data/client_keys_2018_07_10.csv", 
-                    header = TRUE)
-
 # Define UI 
 shinyUI(fluidPage(
   # Application title
-  titlePanel("Staywell: Google Analytics Reporting"),
+  titlePanel("Test reporting app"),
   
-  #Adjust location of progress bars
-  tags$head(tags$style(".shiny-notification {position: fixed; top: 25% ;left: 50%")),
-  
+
 # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      selectInput("client", 
-                  label = h4("Client"),
-                  choices = clients %>% .$client_name %>% unique()),
-      dateInput("start_date", label = h4("Start date"), value = "2017-06-30"), 
-      dateInput("end_date", label = h4("End date")),
+      numericInput("seed", 
+                   label = "Set seed", 
+                   value = 256),
+      selectInput("algo", 
+                  label = h4("RNG algorithm"),
+                  choices = c("Wichmann-Hill", "Marsaglia-Multicarry", "Super Duper", 
+                              "Mersenne-Twister", "Knuth-TAOCP-2002", 
+                              "Knuth-TAOCP", "L'Ecuyer-CMRG")),
+      numericInput("n", 
+                   label = h4("Number of values to generate"),
+                   value = 1000),
       downloadButton("generate", "Generate Report", #icon = icon("file"), 
                      style = "color: #fff; background-color: #337ab7; border-color: #2e6da4")
       ),
     
   # Show a plot of the generated distribution
     mainPanel(
-      p("This app allows users to automatically generate a report using Google Analytics 
-        data from the StayWell Portal for any of the clients in the dropdown menu 
-        on the left. Clients in menu are from Josh Everson. Client lists can be updated easily."), 
-      p("Login information to the Google Analytics account is provided by a 'service' 
-        account and a Google Project. The Google project is tied to the Google Analytics Reporting API.")
+      p("Just trying to recreate beamer .pdf download issues with an app that doesn't have
+        proprietary information.")
     )
   )
 ))
